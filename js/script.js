8c45c4c2268939292a25e18e7e98213b95d640ad
martdev1963 <martdev1963@gmail.com>
2023-04-20 12:45:31 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/**
+ * rm -rf some_dir
+-r "recursive" -f "force" (suppress confirmation messages)
+Be careful!
+--Jim Lewis
+https://stackoverflow.com/questions/2642147/how-to-remove-files-and-directories-quickly-via-terminal-bash-shell#2642161
+*/
